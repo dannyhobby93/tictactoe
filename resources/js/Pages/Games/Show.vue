fc59a9f6c194f8a34431980fbe8ad173cde5dea9
Danny Hobby <danny.hobby@outlook.com>
2024-08-14 16:56:10 +0100

    Join games and adding reverb/echo

--- resources/js/Pages/Games/Show.vue
+++ resources/js/Pages/Games/Show.vue
@@ -1,7 +1,5 @@
 <template>
-    <AuthenticatedLayout>
-        Testing!
-    </AuthenticatedLayout>
+    <AuthenticatedLayout> Testing! {{ $page.props }} </AuthenticatedLayout>
 </template>
 <script setup lang="ts">
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
