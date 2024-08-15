fc8b6cf5bd205d2692823da90dfda8e63be44f6b
Danny Hobby <danny.hobby@outlook.com>
2024-08-15 13:14:37 +0100

    Creating board

--- resources/js/Pages/Games/Show.vue
+++ resources/js/Pages/Games/Show.vue
@@ -1,6 +1,31 @@
 <template>
-    <AuthenticatedLayout> Testing! {{ $page.props }} </AuthenticatedLayout>
+    <AuthenticatedLayout>
+        <menu class="grid grid-cols-3 gap-1.5 w-0 min-w-fit mx-auto mt-12">
+            <li
+                v-for="(square, index) in boardState"
+                class="size-32 grid place-items-center bg-gray-300 text-4xl font-bold"
+            >
+                <button
+                    @click="fillSquare(index)"
+                    v-if="square === 0"
+                    class="place-self-stretch bg-gray-200 hover:bg-gray-300 transition-colors"
+                ></button>
+                <span v-else v-text="square === -1 ? 'X' : 'O'"></span>
+            </li>
+        </menu>
+    </AuthenticatedLayout>
 </template>
 <script setup lang="ts">
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { computed, ref } from "vue";
+
+const boardState = ref([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+const xTurn = computed(
+    () => boardState.value.reduce((carry, value) => carry + value, 0) === 0
+);
+
+const fillSquare = (index) => {
+    boardState.value[index] = xTurn.value ? -1 : 1;
+};
 </script>
